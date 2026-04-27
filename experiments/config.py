5d306e2ce6afe43b70519d34db977c019a40e37a
tanyakumar23 <52919594+tanyakumar23@users.noreply.github.com>
2026-04-27 14:06:31 -0400

    Added Experiment files the correct ones

---
+++ experiments/config.py
@@ -0,0 +1,5 @@
+# change to your path
+NWB_DIR = 'E:/000623'
+SCENECUTS_CSV = 'E:/bmovie-release-NWB-BIDS/assets/annotations/scenecut_info.csv'
+FACEANNOTS_FILE = 'E:/bmovie-release-NWB-BIDS/assets/annotations/short_faceannots.pkl'
+OUT_DIR = './event_boundary_figs'
